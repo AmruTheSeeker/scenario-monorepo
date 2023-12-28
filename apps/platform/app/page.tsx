c228fb839589d450ef7c6cb246a822cb35fc97d1
codelearn <codelearn@WelcomeFuture>
2023-12-28 08:29:07 +0700

    feat: connect BE and FE

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,3 +1,15 @@
+async function getData() {
+  const res = await fetch('http://localhost:8000/api/hello');
+  const data = await res.json();
+  return data; 
+}
+
 export default async function Index() {
-  return <div>Platform</div>;
+  const data = await getData();
+  return (
+    <div>
+      <h3>Platform</h3>
+      <pre>{ JSON.stringify(data, null, 2) }</pre>
+    </div>
+    )
 }
